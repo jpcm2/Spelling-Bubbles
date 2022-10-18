5ccf473172e934c2561bd3b5bbc68b079c28e694
apfdamascena <apfd@cin.ufpe.br>
2022-10-18 19:55:08 -0300

    feature(shake-gesture-recognizer): add gesture recognizer

---
+++ spelling-bubbles/Game/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  joaninos
+//
+//  Created by alexdamascena on 10/10/22.
+//
+
+import SwiftUI
+import SpriteKit
+
+
+struct GameView: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = GameViewController
+    
+    func makeUIViewController(context: Context) -> GameViewController {
+        return GameViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: GameViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        GameView()
+            .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ spelling-bubbles/Game/GameScene+ShakeHandler.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene+ShakeHandler.swift
+//  spelling-bubbles
+//
+//  Created by alexdamascena on 18/10/22.
+//
+
+import Foundation
+
+extension GameScene: ShakeHandler {
+    
+    func didUserStartShake() {
+        print("shake funcionando")
+    }
+}
