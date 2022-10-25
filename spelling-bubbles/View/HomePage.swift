9486593510a8bc9bbc3a9fe3351da0f2723b8edb
miggelucas <lucasmigge@gmail.com>
2022-10-25 15:44:46 -0300

    feature(homePage): first commit

---
+++ spelling-bubbles/View/HomePage.swift
@@ -0,0 +1,22 @@
+//
+//  HomePage.swift
+//  spelling-bubbles
+//
+//  Created by Lucas Migge de Barros on 25/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomePageView : View {
+    
+    var body: some View {
+        Color(.red)
+    }
+}
+
+struct HomePageView_Previews : PreviewProvider {
+    static var previews: some View {
+        HomePageView() 
+    }
+}
