7279a869353de7064137d29042313f6805a68491
Alex Damascena <apfd@cin.ufpe.br>
2022-10-27 02:05:53 -0300

    fix(refactoring): add new function to any node

--- spelling-bubbles/Protocols/AnyNode.swift
+++ spelling-bubbles/Protocols/AnyNode.swift
@@ -9,19 +9,24 @@ import Foundation
 
 
 protocol AnyNode {
+    
     func setupPosition()
     func setupPhysicsBody()
     func setupAdditionalConfiguration()
+    func addChilds()
 }
 
 extension AnyNode {
+    
     func setupNode(){
         setupPosition()
         setupPhysicsBody()
+        addChilds()
         setupAdditionalConfiguration()
     }
     
     func setupAdditionalConfiguration(){}
     func setupPhysicsBody(){}
+    func addChilds(){}
 }
 
